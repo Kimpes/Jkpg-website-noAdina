de84f47722b2ea49fe3204018f7deea90548bb22
Kimpes <gustafssonfilip@hotmail.com>
2024-01-17 11:04:38 +0100

    Added get functionality

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080;
 const path = require('path');
 
-const venues = require('stores.json');
+const venues = require('./stores.json');
 
 app.use(
     express.urlencoded({
@@ -11,8 +11,16 @@ app.use(
     })
 );
 
+//type this in the http client to test: http://localhost:8080/?storename=Akademibokhandeln
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    const {storename} = req.query;
+    const index = venues.findIndex(store => store.name === storename);
+    if (index <= -1) {
+        res.send("oops, silly");
+    } else {
+        res.send(`Index is ${index}`);
+    }
+    //res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
