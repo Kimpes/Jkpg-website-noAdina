51ab238aa4964021b9843d40ae096cd7296a07c6
Saika0k1 <phopat91@gmail.com>
2024-01-22 19:05:34 +0100

    further initial set up

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import { useState } from 'react'
 export default function Home() {
   let [number, setNumber] = useState(0);
 
+  // write html down here, <main> has to wrap around everything
+  // instead of "class", write "className" instead like shown in <button>. it should autofill it automatically
   return (
     <main>
       <h1>You have clicked {number} times!</h1>
-      <button className='border-white border px-4 py-1' onClick={() => setNumber(number + 1)}>
+      <button className='border border-white px-4 py-1' onClick={() => setNumber(number + 1)}>
         Add
       </button>
     </main>
