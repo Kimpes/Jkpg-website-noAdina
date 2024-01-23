cb71f56c93be04d277b44c7207e1fced902007d6
Saika0k1 <phopat91@gmail.com>
2024-01-23 23:10:53 +0100

    tested data fetching

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req, res) {
+  const options = {
+    method: `${req.method}`,
+    url: 'https://catfact.ninja/fact'
+  }
+
+  switch (req.method) {
+    case "GET":
+      try {
+        const response = await axios.request(options);
+        res.status(200).json(response.data);
+      } catch (error) {
+        res.status(404).send("error 404: not found");
+      }
+      break;
+  
+    default:
+      break;
+  }
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 
 export default function Home() {
-  let [number, setNumber] = useState(0);
+  const [data, setData] = useState(null)
+  const [isLoading, setLoading] = useState(true)
+  const [factGenerator, setFact] = useState(0)
+
+  useEffect(() => {
+    setLoading(false);
+
+    fetch('/api/hello')
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data.fact);
+        setLoading(false);
+      })
+  }, [factGenerator])
 
   // write html down here, <main> has to wrap around everything
   // instead of "class", write "className" instead like shown in <button>. it should autofill it automatically
   return (
     <main>
-      <h1>You have clicked {number} times!</h1>
-      <button className='border border-white px-4 py-1' onClick={() => setNumber(number + 1)}>
-        Add
+      <div>
+        { isLoading ? <h5>Loading...</h5> : <h5>{ data }</h5>}
+      </div>
+      <button className='px-4 py-1 border border-white' onClick={() => setFact(factGenerator + 1)}>
+        Generate another fact
       </button>
     </main>
   );
