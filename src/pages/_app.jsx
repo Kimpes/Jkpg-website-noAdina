184043c829a9f91cf0aa5d01096ca2a725ea48ac
Kimpes <gustafssonfilip@hotmail.com>
2024-01-31 10:21:25 +0100

    Trying to fix docker (unsuccesful)

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,5 @@
-import "@/styles/globals.css";
-import Layout from "@/components/layout";
+import "../styles/globals.css";
+import Layout from "../components/layout";
 
 export default function App({ Component, pageProps }) {
   return (
---
+++ src/pages/api/model.jsx
@@ -0,0 +1,56 @@
+import { Client } from 'pg';
+const stores = require("../../../stores.json");
+
+class Model {
+  constructor() {
+    this.client = new Client({
+      user: 'postgress',
+      host: 'localhost',
+      database: 'postgres',
+      password: '123',
+      port: '5431'
+    })
+  }
+
+  async init() {
+    await this.client.connect();
+  }
+
+  async setupDatabase() {
+    await this.connection.query(`
+    CREATE TABLE IF NOT EXISTS public.stores
+    (
+        id SERIAL,
+        name text,
+        url text,
+        district text,
+        type text,
+        CONSTRAINT stores_pkey PRIMARY KEY (id)
+    )`);
+
+    await this.connection.query(`
+      ALTER TABLE IF EXISTS public.stores
+          OWNER to postgres
+    `);
+
+
+    for (const store of stores) {
+      const { rows } = await this.connection.query(`
+        SELECT * FROM stores WHERE name = $1
+      `, [store.name]);
+
+      if (rows.length === 0) {
+        if (!store.type) {
+          store.type = null;
+        }
+        console.log(`Inserting ${store.name}`);
+        await this.connection.query(`
+          INSERT INTO stores (name, url, district, type)
+          VALUES ($1, $2, $3, $4)
+        `, [store.name, store.url, store.district, store.type]);
+      }
+    }
+  }
+}
+
+export default Model;
