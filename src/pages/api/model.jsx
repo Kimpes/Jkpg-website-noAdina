349fe1f3afea7dab433d0ae70161abac5f00151b
Kimpes <gustafssonfilip@hotmail.com>
2024-02-07 10:33:51 +0100

    Added CRUD to model.jxs
    you can now add, edit, delete, and getall

--- src/pages/api/model.jsx
+++ src/pages/api/model.jsx
@@ -39,25 +39,56 @@ class Model {
         SELECT * FROM stores WHERE name = $1
       `, [store.name]);
 
-      if (rows.length === 0) {
+      if (rows.length == 0) {
         if (!store.type) {
           store.type = null;
         }
         console.log(`Inserting ${store.name}`);
         await this.client.query(`
-          INSERT INTO stores (name, url, district, type)
+          INSERT INTO public.stores (name, url, district, type)
           VALUES ($1, $2, $3, $4)
         `, [store.name, store.url, store.district, store.type]);
       }
     }
   }
 
-  async getStoreById(query) {
+  async getStoreById(id) {
     const { rows } = await this.client.query(`
-      SELECT * FROM stores 
+      SELECT * FROM public.stores 
       WHERE id=$1
-      `, [query]);
+      `, [id]);
     
+    return rows[0];
+  }
+
+  async getAllStores() {
+    const { rows } = await this.client.query(`
+      SELECT * FROM public.stores
+      `)
+    return rows
+  }
+
+  async editStore(store) {
+    const { rows } = await this.client.query(`
+      UPDATE public.stores 
+      SET name = $1, url = $2, district = $3, type = $4 
+      WHERE id = $5
+      `, [store.name, store.url, store.district, store.type, store.id])
+    return rows[0];
+  }
+
+  async deleteStore(id) {
+    const { rows } = await this.client.query(`
+      DELETE FROM public.stores WHERE id = $1
+      `, [id])
+    return;
+  }
+
+  async addStore(store) {
+    const { rows } = await this.client.query(`
+      INSERT INTO portfolio_entries (title, description, post_date, tag_1, tag_2, image_name) 
+      VALUES ($1, $2, $3, $4)
+      `, [store.name, store.url, store.district, store.type])
     return rows[0];
   }
 }
