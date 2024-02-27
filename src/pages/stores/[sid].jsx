695789bd83baf6d83d88cb26c26d02a4f79086b2
Kimpes <gustafssonfilip@hotmail.com>
2024-02-27 15:15:55 +0100

    added long page (and fixed a typo)

--- src/pages/stores/[sid].jsx
+++ src/pages/stores/[sid].jsx
@@ -3,14 +3,14 @@ import { useEffect, useState } from "react";
 
 export default function Page() {
   const router = useRouter();
-  const [store, setStore] = useState([]);
+  const [store, setStore] = useState([]); //could we make it an object instead of an array?
 
   useEffect(() => {
     async function fetchData() {
       if (!router.isReady) return;
 
       await fetch("/api/stores/controller", {
-        method: "POST",
+        method: "POST", //i mean, it's not a post request really, so we should change this
         headers: {
           "Content-Type": "application/json",
         },
@@ -33,6 +33,7 @@ export default function Page() {
         <li>{store[2]}</li>
         <li>{store[3]}</li>
       </ul>
+      <button className="primary-button">Edit store</button>
     </div>
   );
 }
