bebb66f73b211f49e1df578e2f94d8ef92902456
Kimpes <gustafssonfilip@hotmail.com>
2024-03-05 12:52:05 +0100

    Added a simple footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,68 @@
 export default function Footer() {
   return (
     <footer>
-      <p>
-        Footer
-      </p>
+      <div className="footer-grid">
+        <div>
+          <h3>jkpgcity.</h3>
+          <p>Stuff</p>
+          <p>Thing</p>
+          <p>Adress</p>
+          <p>Email</p>
+          <p>Information</p>
+        </div>
+        <div className="footer-grid-links">
+          <ul>
+            <li>
+              <a href="/">Upptäck</a>
+            </li>
+            <li>
+              <a href="/">Vandra</a>
+            </li>
+            <li>
+              <a href="/">Evenemang</a>
+            </li>
+            <li>
+              <a href="/">Stadsdelar</a>
+            </li>
+            <li>
+              <a href="/">Information</a>
+            </li>
+            <li>
+              <a href="/">Aktör</a>
+            </li>
+            <li>
+              <a href="/">Kontakt</a>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <a href="/">Upptäck</a>
+            </li>
+            <li>
+              <a href="/">Vandra</a>
+            </li>
+            <li>
+              <a href="/">Evenemang</a>
+            </li>
+            <li>
+              <a href="/">Stadsdelar</a>
+            </li>
+            <li>
+              <a href="/">Information</a>
+            </li>
+            <li>
+              <a href="/">Aktör</a>
+            </li>
+            <li>
+              <a href="/">Kontakt</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <p>Copyright reserved to JKPG city</p>
+        <a href="#">integritetspolicy</a>
+      </div>
     </footer>
-  )
+  );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,25 +1,24 @@
 // dependencies import
-import { useState, useEffect } from 'react';
-import dynamic from 'next/dynamic';
+import { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
 
 export default function Home() {
-  const [store, setStore] = useState(null)
+  const [store, setStore] = useState(null);
 
   useEffect(() => {
-    fetch('/api/events/controller')
+    fetch("/api/events/controller");
 
     // console.log(store.name)
-    
-  }, [])
+  }, []);
 
   // write html down here, <main> has to wrap around everything
   // instead of "class", write "className" instead like shown in <button>. it should autofill it automatically
   return (
-    <main>
+    <div className="main-content">
       <div>
-        <p>here's some text</p>
+        <h1>Welcome to Jönköping City</h1>
       </div>
       <a href="/long-page">Go to the long page</a>
-    </main>
+    </div>
   );
 }
