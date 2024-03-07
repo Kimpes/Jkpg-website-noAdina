3db48f0e5f307594ee5dde0a0052913eda73d489
Mokyuumokyuu <93583916+Mokyuumokyuu@users.noreply.github.com>
2024-03-07 12:50:14 +0100

    made buttons on frontpage

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,6 +19,42 @@ export default function Home() {
         <h1>Welcome to Jönköping City</h1>
       </div>
       <a href="/long-page">Go to the long page</a>
+      <div className="page-selection">
+        <h2>Verksamheter på Tändsticksområdet</h2>
+        <h3>Vad vill du göra?</h3>
+        <ul className="page-selection-buttons">
+          <li>
+            <a href="#">
+              <img src="/assets/square-placeholder.svg" alt="" />
+              <h4>Shoppa</h4>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="/assets/square-placeholder.svg" alt="" />
+              <h4>Må Bra</h4>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="/assets/square-placeholder.svg" alt="" />
+              <h4>Upplev</h4>
+            </a>
+          </li>
+          <li>
+          <a href="#">
+              <img src="/assets/square-placeholder.svg" alt="" />
+              <h4>Ata</h4>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src="/assets/square-placeholder.svg" alt="" />
+              <h4>Sova</h4>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
