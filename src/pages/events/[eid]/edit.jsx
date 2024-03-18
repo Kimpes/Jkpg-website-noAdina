385678902528cfe6a6671f27b5e8cfe32b7f9ac4
Mokyuumokyuu <93583916+Mokyuumokyuu@users.noreply.github.com>
2024-03-18 19:05:46 +0100

    Event page update

--- src/pages/events/[eid]/edit.jsx
+++ src/pages/events/[eid]/edit.jsx
@@ -4,11 +4,11 @@ import { useEffect, useState } from "react";
 export default function Page() {
   const router = useRouter();
   const [event, setEvent] = useState([]); //could we make it an object instead of an array?
-  const [title, setTitle] = useState('')
-  const [description, setDescription] = useState('')
-  const [location, setLocation] = useState('')
-  const [type, setType] = useState('')
-  const [date, setDate] = useState('')
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [location, setLocation] = useState("");
+  const [type, setType] = useState("");
+  const [date, setDate] = useState("");
 
   useEffect(() => {
     // console.log(queryParams)
@@ -16,7 +16,7 @@ export default function Page() {
       if (!router.isReady) return;
 
       await fetch("/api/events/controller", {
-        method: "PUT", 
+        method: "PUT",
         headers: {
           "Content-Type": "application/json",
         },
@@ -24,14 +24,20 @@ export default function Page() {
       })
         .then((res) => res.json())
         .then((data) => {
-          setEvent([data.id, data.title, data.description, data.location, data.type, data.date])
-          setTitle(data.title)
-          setDescription(data.description)
-          setLocation(data.location)
-          setType(data.type)
-          setDate(data.date)
-        })
-      
+          setEvent([
+            data.id,
+            data.title,
+            data.description,
+            data.location,
+            data.type,
+            data.date,
+          ]);
+          setTitle(data.title);
+          setDescription(data.description);
+          setLocation(data.location);
+          setType(data.type);
+          setDate(data.date);
+        });
     }
 
     fetchData();
@@ -46,66 +52,82 @@ export default function Page() {
       description,
       location,
       type,
-      date
-    }
-    
-    const response = await fetch('/api/events/controller', {
+      date,
+    };
+
+    const response = await fetch("/api/events/controller", {
       method: "PATCH",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(altered),
-    }).then(router.push(`/events/${event[0]}`))
+    }).then(router.push(`/events/${event[0]}`));
   }
 
   return (
     <main>
-      <form onSubmit={ handleSubmit }>
+      <form onSubmit={handleSubmit}>
         <ul>
-          <li>ID: { event[0] } </li>
-          <li>Title:
-            <input 
-            required 
-            type="text" 
-            onChange={ (e) => setTitle(e.target.value) }
-            defaultValue={ event[1] }
-            />  
+          <li>ID: {event[0]} </li>
+          <li>
+            Title:
+            <input
+              required
+              type="text"
+              onChange={(e) => setTitle(e.target.value)}
+              defaultValue={event[1]}
+            />
           </li>
-          <li>Description:
-          <textarea
-            required
-            name='description'
-            onChange={ (e) => setDescription(e.target.value) }
-            defaultValue={ event[2] }
-            />  
+          <li>
+            Description:
+            <textarea
+              required
+              name="description"
+              onChange={(e) => setDescription(e.target.value)}
+              defaultValue={event[2]}
+            />
           </li>
-          <li>Location: 
-          <input 
-            type="text" 
-            onChange={ (e) => setLocation(e.target.value) }
-            defaultValue={ event[3] }
-            />  
+          <li>
+            Location:
+            <input
+              type="text"
+              onChange={(e) => setLocation(e.target.value)}
+              defaultValue={event[3]}
+            />
           </li>
-          <li>Type: 
-          <input 
-            type="text" 
-            onChange={ (e) => setType(e.target.value) }
-            defaultValue={ event[4] }
-            />  
+          <li>
+            Type:
+            <input
+              type="text"
+              onChange={(e) => setType(e.target.value)}
+              defaultValue={event[4]}
+            />
           </li>
-          <li>Date: 
-          <input 
-            type="date" 
-            onChange={ (e) => setDate(e.target.value) }
-            defaultValue={ event[5] }
-            />  
+          <li>
+            Date:
+            <input
+              type="date"
+              onChange={(e) => setDate(e.target.value)}
+              defaultValue={event[5]}
+            />
           </li>
         </ul>
         <div className="buttons-container">
-          <input className="primary-button" type="submit" name="submit" id="submit" value="Save Changes" />
-          <button className="danger-button" onClick={() => router.push(`/stores/${store[0]}`)}>Cancel</button>
+          <input
+            className="primary-button"
+            type="submit"
+            name="submit"
+            id="submit"
+            value="Save Changes"
+          />
+          <button
+            className="danger-button"
+            onClick={() => router.push(`/events/${event[0]}`)}
+          >
+            Cancel
+          </button>
         </div>
       </form>
     </main>
-  )
+  );
 }
--- src/pages/events/[eid]/index.jsx
+++ src/pages/events/[eid]/index.jsx
@@ -5,6 +5,8 @@ export default function Page() {
   const router = useRouter();
   const [event, setEvent] = useState([]);
 
+  console.log(event);
+
   useEffect(() => {
     async function fetchData() {
       if (!router.isReady) return;
@@ -18,26 +20,94 @@ export default function Page() {
       })
         .then((res) => res.json())
         .then((data) =>
-          setEvent([data.id, data.title, data.description, data.location, data.type, data.date])
+          setEvent([
+            data.id,
+            data.title,
+            data.description,
+            data.location,
+            data.type,
+            data.date,
+          ])
         );
     }
 
     fetchData();
   }, [router.isReady]);
 
+  async function deleteEvent() {
+    await fetch("/api/events/controller", {
+      method: "DELETE",
+      body: JSON.stringify(router.query),
+    });
+
+    router.push("/events");
+  }
+
   return (
     <div>
-      <h1>{event[1]}</h1>
-      <ul>
-        <li>{event[0]}</li>
-        <li>{event[2]}</li>
-        <li>{event[3]}</li>
-        <li>{event[4]}</li>
-        <li>{event[5]}</li>
+      <div className="herosection item-page">
+        <div className="page-header">
+          <span>
+            <h1>{event[1] /*Event name*/}</h1>
+            <button
+              className="primary-button"
+              onClick={() => router.push(`/events/${event[0]}/edit`)}
+            >
+              edit
+            </button>
+          </span>
+        </div>
+      </div>
+      <ul className="page-selection-buttons">
+        <div className="primary-button">
+          <li>
+            <a>
+              <div className="image">
+                <img src={`/assets/${event[3]}.png`} alt="" />
+              </div>
+              <h4>{event[3]}</h4>
+            </a>
+          </li>
+        </div>
+        <div className="primary-button">
+          <li>
+            <a href="">
+              <div className="image">
+                <img src={`/assets/sova.png`} alt="" />
+              </div>
+              <h4>Sign Up</h4>
+            </a>
+          </li>
+        </div>
+        <div className="primary-button">
+          <li>
+            <a href={`https://${event[4]}`}>
+              <img src="/assets/hitta.png" alt="" />
+              <h4>Location</h4>
+            </a>
+          </li>
+        </div>
+        {/* <div className="button-delete">
+          <li>
+            <a href="" onClick={() => deleteStore(store[0])}>
+              <img src="/assets/Link.png" alt="" />
+              <h4>Delete</h4>
+            </a>
+          </li>
+        </div> */}
       </ul>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce orci sem,
+        lacinia ut magna gravida, ornare venenatis felis. Integer in augue sit
+        amet dolor sodales auctor sed vitae augue. Vestibulum posuere non libero
+        id venenatis. Suspendisse eu leo elementum, tincidunt justo id, finibus
+        nunc. Suspendisse potenti.
+      </p>
+
       <div className="buttons-container">
-        <button className="primary-button" onClick={() => router.push(`/events/${ event[0] }/edit`)}>Edit event</button>
-        <button className="danger-button" onClick={() => deleteStore()}>Delete event</button>
+        <button className="danger-button" onClick={() => deleteEvent()}>
+          Delete store
+        </button>
       </div>
     </div>
   );
